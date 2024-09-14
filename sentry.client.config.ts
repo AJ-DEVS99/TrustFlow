95cac3f7ac86311df4f9d35327a90260a8900b76
AJ-DEVS <akhmadjon331@gmail.com>
2024-09-14 16:16:01 +0900

    sentry error handled

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,7 +8,7 @@ Sentry.init({
   dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
